7210d0f54d7cb257791d52d5c61adbe75701f185
Jake Boeckerman <jakebman@gmail.com>
2021-06-07 18:25:13 -0600

    generated from maven archetype

---
+++ protobuf-example/src/main/java/com/boeckerman/jake/protobuf/App.java
@@ -0,0 +1,13 @@
+package com.boeckerman.jake.protobuf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
