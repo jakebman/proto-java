e349b19f04ee953b862f800ae7133e5617ff9979
Jake Boeckerman <jakebman@gmail.com>
2021-06-21 01:12:47 -0600

    Code cleanup

--- plugin/src/main/java/com/boeckerman/jake/protobuf/CodeGeneratorImpl.java
+++ plugin/src/main/java/com/boeckerman/jake/protobuf/CodeGeneratorImpl.java
@@ -3,6 +3,7 @@ package com.boeckerman.jake.protobuf;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,6 +12,7 @@ import static com.google.protobuf.compiler.PluginProtos.*;
 import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.*;
 
 public class CodeGeneratorImpl implements CodeGenerator {
+    private static final int SHORT_LIST_TO_SEARCH_QUICKLY = 12;
     private final CodeGeneratorRequest request;
 
     public CodeGeneratorImpl(CodeGeneratorRequest request) {
@@ -27,16 +29,30 @@ public class CodeGeneratorImpl implements CodeGenerator {
 
 
     private List<File> modifications(CodeGeneratorRequest request) {
-        Map<String, FileDescriptorProto> lookup = new HashMap<>();
-        request.getProtoFileList()
-                .forEach(fileDescriptorProto -> lookup.put(fileDescriptorProto.getName(), fileDescriptorProto));
-
         return request
                 .getFileToGenerateList() // list of .proto file names to work with
                 .stream()
-                .map(lookup::get)
+                .map(generateLookupMapFor(request.getProtoFileList()))
                 .flatMap(this::modifications)
                 .collect(Collectors.toList());
+    }
+
+    private Function<String, FileDescriptorProto> generateLookupMapFor(List<FileDescriptorProto> protoFileList) {
+//       Will never be useful - too many messages files normally (need library files)
+//        if(protoFileList.size() < SHORT_LIST_TO_SEARCH_QUICKLY) {
+//            return (name) -> {
+//                for(FileDescriptorProto p : protoFileList) {
+//                    if (p.getName().equals(name)) {
+//                        return p;
+//                    }
+//                }
+//                return null;
+//            };
+//        }
+        Map<String, FileDescriptorProto> lookup = new HashMap<>();
+        request.getProtoFileList()
+                .forEach(fileDescriptorProto -> lookup.put(fileDescriptorProto.getName(), fileDescriptorProto));
+        return lookup::get;
     }
 
     private Stream<File> modifications(FileDescriptorProto fileDescriptorProto) {
@@ -47,7 +63,7 @@ public class CodeGeneratorImpl implements CodeGenerator {
     }
 
     private File addInterface(FileDescriptorProto fileDescriptorProto,
-                                                           DescriptorProto descriptorProto) {
+                              DescriptorProto descriptorProto) {
         return File.newBuilder()
                 .setName(CodeGeneratorUtils.fileToModify(fileDescriptorProto, descriptorProto))
                 .setInsertionPoint("message_implements:" + CodeGeneratorUtils.insertionPointTypename(descriptorProto, fileDescriptorProto))
