0f3fbe015996b8a49107c6331d4564d87127b8b3
Jake Boeckerman <jakebman@gmail.com>
2021-06-20 22:53:54 -0600

    Got plugin working!
    Plus some pom cleanup

--- plugin/src/main/java/com/boeckerman/jake/protobuf/Plugin.java
+++ plugin/src/main/java/com/boeckerman/jake/protobuf/Plugin.java
@@ -1,42 +1,70 @@
 package com.boeckerman.jake.protobuf;
 
-import com.google.protobuf.DescriptorProtos;
 import com.google.protobuf.compiler.PluginProtos;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.google.protobuf.DescriptorProtos.*;
+import static com.google.protobuf.compiler.PluginProtos.*;
+import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.*;
+
 public class Plugin {
     public static void main(String[] args) throws IOException {
-        PluginProtos.CodeGeneratorRequest request = PluginProtos.CodeGeneratorRequest.parseFrom(System.in);
+        CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(System.in);
 
-        PluginProtos.CodeGeneratorResponse
-                .newBuilder()
+        newBuilder()
                 .addAllFile(modifications(request))
                 .build()
                 .writeTo(System.out);
 
     }
 
-    private static List<PluginProtos.CodeGeneratorResponse.File> modifications(PluginProtos.CodeGeneratorRequest request) {
+    private static List<File> modifications(CodeGeneratorRequest request) {
+        Map<String, FileDescriptorProto> lookup = new HashMap<>();
+        request.getProtoFileList()
+                .forEach(fileDescriptorProto -> lookup.put(fileDescriptorProto.getName(), fileDescriptorProto));
         return request
-                .getProtoFileList()
+                .getFileToGenerateList()
                 .stream()
-                .flatMap(fileDescriptorProto -> modifications(fileDescriptorProto, request))
+                .flatMap(filename -> modifications(filename, lookup.get(filename), request))
                 .collect(Collectors.toList());
     }
 
-    private static Stream<PluginProtos.CodeGeneratorResponse.File> modifications(DescriptorProtos.FileDescriptorProto fileDescriptorProto, PluginProtos.CodeGeneratorRequest request) {
-        return fileDescriptorProto.getMessageTypeList().stream().map(Plugin::addInterface);
+    private static Stream<File> modifications(String filename,
+                                              FileDescriptorProto fileDescriptorProto, CodeGeneratorRequest request) {
+        return fileDescriptorProto
+                .getMessageTypeList()
+                .stream()
+                .map(descriptorProto -> addInterface(filename, fileDescriptorProto, descriptorProto));
     }
 
-    private static PluginProtos.CodeGeneratorResponse.File addInterface(DescriptorProtos.DescriptorProto descriptorProto) {
-        return PluginProtos.CodeGeneratorResponse.File.newBuilder()
-                .setName(descriptorProto.getName())
-                .setContent("// LOL, I can modify! -Jake")
+    private static File addInterface(String filename,
+                                     FileDescriptorProto fileDescriptorProto,
+                                     DescriptorProto descriptorProto) {
+        return File.newBuilder()
+                .setName(fileToModify(fileDescriptorProto, descriptorProto))
+                .setInsertionPoint("message_implements:" + descriptorProto.getName())
+                .setContent("java.io.Serializable, // LOL, I can modify! -Jake")
                 .build();
+    }
+
+    /* TODO: how do we behave correctly in the following situations
+     * No java package
+     * Java wrapper class?
+     */
+    private static String fileToModify(FileDescriptorProto fileDescriptorProto,
+                                       DescriptorProto descriptorProto) {
+        FileOptions options = fileDescriptorProto.getOptions();
+        String javaPackage = options.hasJavaPackage() ? options.getJavaPackage() : fileDescriptorProto.getPackage();
+        List<String> strings = new ArrayList<>(Arrays.asList(javaPackage.split("\\.")));
+        strings.add(descriptorProto.getName() + ".java");
+        return strings.stream().filter(StringUtils::isNotEmpty).collect(Collectors.joining("/"));
+        //sometimes the first part of the path is the empty string. So we can't do this:
+        //return String.join("/", strings);
     }
 
 }
