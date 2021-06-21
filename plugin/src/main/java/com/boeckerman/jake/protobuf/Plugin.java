99c187da2c47e6868b777f9a9894b8aad2ba4031
Jake Boeckerman <jakebman@gmail.com>
2021-06-21 01:02:29 -0600

    Optimize Imports

--- plugin/src/main/java/com/boeckerman/jake/protobuf/Plugin.java
+++ plugin/src/main/java/com/boeckerman/jake/protobuf/Plugin.java
@@ -1,16 +1,8 @@
 package com.boeckerman.jake.protobuf;
 
-import org.apache.commons.lang3.text.WordUtils;
-
 import java.io.IOException;
-import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static com.google.protobuf.DescriptorProtos.*;
-import static com.google.protobuf.compiler.PluginProtos.*;
-import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.*;
+import static com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
 
 public class Plugin {
     public static void main(String[] args) throws IOException {
