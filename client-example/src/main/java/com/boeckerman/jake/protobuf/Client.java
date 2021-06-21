99f7ad33add3c33e04fc26a7e0588337cc6a456a
Jake Boeckerman <jake.boeckerman@charter.com>
2021-06-21 11:45:53 -0600

    Merge in things from the demo to Dipesh and Nick

--- client-example/src/main/java/com/boeckerman/jake/protobuf/Client.java
+++ client-example/src/main/java/com/boeckerman/jake/protobuf/Client.java
@@ -3,6 +3,8 @@ package com.boeckerman.jake.protobuf;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 
+import java.util.List;
+
 public class Client {
     public static void main(String[] args) throws InvalidProtocolBufferException {
         Person person = Person.newBuilder().build();
@@ -14,5 +16,25 @@ public class Client {
                         .setId(23423)
                 ));
 
+        Person2 foo = Person2.newBuilder().setName("23").setId(4322).addDescription("This is a description").build();
+
+        Person2.Builder bar = Person2.newBuilder();
+        bar.setId(23);
+        bar.setName("23433");
+        System.out.println(bar.getId());
+
+        byte[] serialized = serialize(bar);
+        System.out.println(serialized);
+//        Person2 built = Person2.newBuilder().mergeFrom(serialized)
+//                .setShortCutForCOmplexSubFieldStillUsingKryo(Kryo.serialize(thin.getComplexSubfield()))
+//                .build();
+//        System.out.println(built);
+
+        List<String> descriptionList = foo.getDescriptionList();
+        System.out.println(descriptionList);
+    }
+
+    private static byte[] serialize(Person2.Builder bar) {
+        return bar.build().toByteArray();
     }
 }
