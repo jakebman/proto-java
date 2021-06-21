25cb80c3b024fe6aeb0d16404f3c87a51d8c3212
Jake Boeckerman <jake.boeckerman@charter.com>
2021-06-21 13:30:50 -0600

    Rewrite CamelCase to cover most/all cases

--- plugin/src/main/java/com/boeckerman/jake/protobuf/CodeGeneratorUtils.java
+++ plugin/src/main/java/com/boeckerman/jake/protobuf/CodeGeneratorUtils.java
@@ -1,6 +1,6 @@
 package com.boeckerman.jake.protobuf;
 
-import org.apache.commons.lang3.text.WordUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.regex.Pattern;
 
@@ -36,18 +36,50 @@ public class CodeGeneratorUtils {
         } else if (options.hasJavaOuterClassname()) {
             out.append(options.getJavaOuterClassname());
         } else {
-            out.append(CamelCase(descriptorProto.getName()))
-                    .append("OuterClass");
+            out.append(classNameForFile(fileDescriptorProto));
         }
         out.append(".java");
         return out.toString();
+    }
+
+    private static final Pattern TRAILING_PROTO_SUFFIX = Pattern.compile("\\.proto$");
+    private static String classNameForFile(FileDescriptorProto fileDescriptorProto) {
+        String guess = CamelCase(deleteMatchesOfPattern(TRAILING_PROTO_SUFFIX, fileDescriptorProto.getName())); // TODO: compile regex
+
+        // minor concern: This might be inefficient.
+        // If our program is slow, we should count executions
+        if(fileDescriptorProto.getMessageTypeList()
+                .stream()
+                .map(CodeGeneratorUtils::classNameForMessageDescriptor)
+                .noneMatch(guess::equals)) {
+            return guess;
+        } else {
+            return guess + "OuterClass";
+        }
+    }
+
+    private static String deleteMatchesOfPattern(Pattern pattern, String string) {
+        return pattern.matcher(string).replaceAll("");
+    }
+
+    private static String classNameForMessageDescriptor(DescriptorProto descriptorProto) {
+        return CamelCase(descriptorProto.getName());
     }
 
     private static String packageToPath(String javaPackage) {
         return SINGLE_DOT.matcher(javaPackage).replaceAll("/");
     }
 
-    static String CamelCase(String name_with_underscores) {
-        return WordUtils.capitalizeFully(name_with_underscores, '_');
+    // \b is a "word barrier" - zero-width match between a letter and a non-letter
+    // There might be some hilarious interaction between what \b thinks is a letter and what
+    // IsAlphabetic thinks is a letter. Regex on unicode is fun.
+    static final Pattern CAPITALIZABLE_CHARACTER = Pattern.compile("\\b(\\p{IsAlphabetic})");
+    static final Pattern NON_ALPHABETICAL_CHARACTER = Pattern.compile("[^\\p{IsAlphabetic}]+");
+
+    static String CamelCase(String name_with_underscoresOrHyphens) {
+        String nameWithCorrectCaps = CAPITALIZABLE_CHARACTER.matcher(name_with_underscoresOrHyphens)
+                .replaceAll(matchResult -> StringUtils.capitalize(matchResult.group()));
+        String nameWithoutNonCharacters = deleteMatchesOfPattern(NON_ALPHABETICAL_CHARACTER, nameWithCorrectCaps);
+        return nameWithoutNonCharacters;
     }
 }
