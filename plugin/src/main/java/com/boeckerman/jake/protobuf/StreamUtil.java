622787a30ad55168d2922a727399ff005b5d9cd9
Jake Boeckerman <jake.boeckerman@charter.com>
2021-07-08 09:15:11 -0600

    Fix warning with safe varargs. This is safe.

--- plugin/src/main/java/com/boeckerman/jake/protobuf/StreamUtil.java
+++ plugin/src/main/java/com/boeckerman/jake/protobuf/StreamUtil.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class StreamUtil {
+    // Useful for ordered streams. Not useful for unordered streams.
     // https://www.techempower.com/blog/2016/10/19/efficient-multiple-stream-concatenation-in-java/
 
     // provided as a base case, and to allow for source code to just reference StreamUtil.concat
@@ -23,6 +24,7 @@ public class StreamUtil {
         return Arrays.stream(streams).flatMap(Function.identity());
     }
 
+    @SafeVarargs
     static <T> Stream<T> concat(Supplier<Stream<T>>... streams) {
         return Arrays.stream(streams).flatMap(Supplier::get);
     }
